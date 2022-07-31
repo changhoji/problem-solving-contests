0d34c9ea1cea88315d231b38ad743ce1bf5c3e7c
Changho Lee <onkkno1@gmail.com>
2022-08-01 01:21:08 +0900

    solving

---
+++ Codeforces/809 (Div. 2)/D1. Chopping Carrots (Easy Version)/source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int a[3000];
+int p[3000];
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
+        int n, k;
+        cin >> n >> k;
+    }
+}
