6029c4a32d99aed3375a26dbcc148c83286e8a94
Changho Lee <onkkno1@gmail.com>
2022-08-02 01:59:41 +0900

    solving

---
+++ Codeforces/811 (Div. 3)/D. Color with Occurrences/source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    
+    for (int tc = 1; tc <= q; tc++) {
+        string t;
+        cin >> t;
+
+        int n;
+        cin >> n;
+
+        string s[10];
+        for (int i = 0; i < n; i++) {
+            cin >> s[i];
+        }
+
+        
+    }
+}
